04c05688b8e3daa7d21121dfe0b4400ac4513d01
Akanksha <akankshazc23@icloud.com>
2025-09-04 22:39:49 +0530

    Refactor Shuffle1.java to improve code readability and maintain expected output

--- Shuffle1.java
+++ Shuffle1.java
@@ -1,7 +1,11 @@
+// Head First Java
+// Chapter 1 Code Magnets: Expected output should be "a-b c-d" 
+
 class Shuffle1 {
     public static void main(String[] args) {
         int x = 3;
         while (x > 0) {
+
             if (x > 2) {
                 System.out.print("a");
             }
@@ -12,9 +16,6 @@ class Shuffle1 {
             if (x == 2) {
                 System.out.print("b c");
             }
-
-            x = x - 1;
-            System.out.print("-");
 
             if (x == 1) {
                 System.out.print("d");
