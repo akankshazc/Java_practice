d400a925adfd648e9b14a0aa3b018e156b059bbf
Akanksha <akankshazc23@icloud.com>
2025-09-06 23:18:01 +0530

    Refactor Button1.java to implement a GUI with multiple buttons in different positions

--- Button1.java
+++ Button1.java
@@ -1,3 +1,5 @@
+// Chapter 15
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -10,16 +12,20 @@ public class Button1 {
 
     public void go() {
         JFrame frame = new JFrame();
-        JButton button1 = new JButton("click like you mean it");
-        frame.getContentPane().add(BorderLayout.EAST, button1);
-        frame.setSize(200, 200);
-        frame.setVisible(true);
 
-        JButton button2 = new JButton("There is no spoon...");
-        Font bigFont = new Font("serif", Font.BOLD, 28);
-        button2.setFont(bigFont);
-        frame.getContentPane().add(BorderLayout.NORTH, button2);
-        frame.setSize(200, 200);
+        JButton east = new JButton("East");
+        JButton west = new JButton("West");
+        JButton north = new JButton("North");
+        JButton south = new JButton("South");
+        JButton center = new JButton("Center");
+
+        frame.getContentPane().add(BorderLayout.EAST, east);
+        frame.getContentPane().add(BorderLayout.WEST, west);
+        frame.getContentPane().add(BorderLayout.NORTH, north);
+        frame.getContentPane().add(BorderLayout.SOUTH, south);
+        frame.getContentPane().add(BorderLayout.CENTER, center);
+
+        frame.setSize(300, 300);
         frame.setVisible(true);
     }
 
