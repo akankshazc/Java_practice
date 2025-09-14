d98817ab9f76946e3aebe27f2822666fac45c560
Akanksha <akankshazc23@icloud.com>
2025-09-14 22:17:23 +0530

    Add Panel1.java to implement a basic GUI with buttons using Swing

---
+++ Panel1.java
@@ -0,0 +1,32 @@
+// Chapter 15
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Panel1 {
+
+    public static void main(String[] args) {
+        Panel1 gui = new Panel1();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.darkGray);
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JButton button = new JButton("shock me");
+        JButton buttonTwo = new JButton("bliss");
+        JButton buttonThree = new JButton("huh?");
+
+        panel.add(button);
+        panel.add(buttonTwo);
+        panel.add(buttonThree);
+
+        frame.getContentPane().add(BorderLayout.EAST, panel);
+        frame.setSize(200, 200);
+        frame.setVisible(true);
+    }
+}
