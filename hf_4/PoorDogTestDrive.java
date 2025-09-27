aefcf0e3a39ed7eda91d14d0a7a93967961de8f8
Akanksha <akankshazc23@icloud.com>
2025-09-27 22:59:07 +0530

    Add PoorDog and PoorDogTestDrive classes to demonstrate encapsulation and default values

---
+++ hf_4/PoorDogTestDrive.java
@@ -0,0 +1,23 @@
+package hf_4;
+// Instance variables always get a default value
+
+class PoorDog {
+    private int size;
+    private String name;
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class PoorDogTestDrive {
+    public static void main(String[] args) {
+        PoorDog one = new PoorDog();
+        System.out.println("Dog size is " + one.getSize());
+        System.out.println("Dog name is " + one.getName());
+    }
+}
