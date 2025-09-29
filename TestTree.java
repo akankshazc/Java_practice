b6b65bd1ff7f77d131ff086fdf0d807b16edc97a
Akanksha <akankshazc23@icloud.com>
2025-09-29 22:57:29 +0530

    Add TestTree class to demonstrate usage of TreeSet with custom Book objects

---
+++ TestTree.java
@@ -0,0 +1,42 @@
+// Chapter 11
+
+import java.util.*;
+
+public class TestTree {
+    public static void main(String[] args) {
+        new TestTree().go();
+    }
+
+    public void go() {
+        Book b1 = new Book("How Cats Work");
+        Book b2 = new Book("Remix your Body");
+        Book b3 = new Book("Finding Emo");
+
+        Set<Book> tree = new TreeSet<>();
+        tree.add(b1);
+        tree.add(b2);
+        tree.add(b3);
+
+        System.out.println(tree);
+    }
+}
+
+class Book implements Comparable<Book> {
+    private String title;
+
+    public Book(String t) {
+        title = t;
+    }
+
+    public int compareTo(Book b) {
+        return title.compareTo(b.getTitle());
+    }
+
+    public String toString() {
+        return title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
