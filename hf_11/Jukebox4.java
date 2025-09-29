4208553353a11b1381b040aefc6a2440cddb49d4
Akanksha <akankshazc23@icloud.com>
2025-09-29 22:55:03 +0530

    Add Jukebox4 class to demonstrate song list management and sorting using Comparator

---
+++ hf_11/Jukebox4.java
@@ -0,0 +1,29 @@
+package hf_11;
+
+import java.util.*;
+
+public class Jukebox4 {
+    public static void main(String[] args) {
+        new Jukebox4().go();
+    }
+
+    public void go() {
+        List<SongV3> songList = MockSongs.getSongsV3();
+        System.out.println(songList);
+
+        Collections.sort(songList);
+        System.out.println(songList);
+
+        ArtistCompare artistCompare = new ArtistCompare();
+        songList.sort(artistCompare);
+        System.out.println(songList);
+    }
+}
+
+class ArtistCompare implements Comparator<SongV3> {
+    public int compare(SongV3 one, SongV3 two) {
+        return one.getArtist().compareTo(two.getArtist());
+    }
+}
+
+// the SongV3 and MockSongs class come from Jukebox3.java
