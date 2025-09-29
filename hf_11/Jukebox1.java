c517a32a62d1dab1ab2340f6c7079815dad2f9f4
Akanksha <akankshazc23@icloud.com>
2025-09-29 22:54:09 +0530

    Add Jukebox1 class to demonstrate song list management and sorting

---
+++ hf_11/Jukebox1.java
@@ -0,0 +1,29 @@
+package hf_11;
+
+import java.util.*;
+
+public class Jukebox1 {
+    public static void main(String[] args) {
+        new Jukebox1().go();
+    }
+
+    public void go() {
+        List<String> songList = MockSongs.getSongStrings();
+        System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
+    }
+}
+
+class MockSongs {
+    public static List<String> getSongStrings() {
+        List<String> songs = new ArrayList<>();
+        songs.add("somersault");
+        songs.add("cassidy");
+        songs.add("$10");
+        songs.add("havana");
+        songs.add("Cassidy");
+        songs.add("50 ways");
+        return songs;
+    }
+}
