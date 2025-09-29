f4a64d4b4e83e65194d187154af8e11234aab230
Akanksha <akankshazc23@icloud.com>
2025-09-29 22:54:20 +0530

    Add Jukebox2 class to demonstrate song list management and sorting

---
+++ hf_11/Jukebox2.java
@@ -0,0 +1,62 @@
+package hf_11;
+// this does not compile because SongV2 doesn't implement comparable.
+
+// solved in Jukebox3
+
+import java.util.*;
+
+public class Jukebox2 {
+    public static void main(String[] args) {
+        new Jukebox2().go();
+    }
+
+    public void go() {
+        List<SongV2> songList = MockSongs.getSongsV2();
+        System.out.println(songList);
+
+        Collections.sort(songList);
+        System.out.println(songList);
+    }
+}
+
+class SongV2 {
+    private String title;
+    private String artist;
+    private int bpm;
+
+    // SongV2 Constructor
+    SongV2(String title, String artist, int bpm) {
+        this.title = title;
+        this.artist = artist;
+        this.bpm = bpm;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getBpm() {
+        return bpm;
+    }
+
+    public String toString() {
+        return title;
+    }
+}
+
+class MockSongs {
+    public static List<SongV2> getSongsV2() {
+        List<SongV2> songs = new ArrayList<>();
+        songs.add(new SongV2("somersault", "zero 7", 147));
+        songs.add(new SongV2("cassidy", "grateful dead", 158));
+        songs.add(new SongV2("$10", "hitchhiker", 140));
+        songs.add(new SongV2("havana", "cabello", 105));
+        songs.add(new SongV2("Cassidy", "grateful dead", 158));
+        songs.add(new SongV2("50 ways", "simon", 102));
+        return songs;
+    }
+}
