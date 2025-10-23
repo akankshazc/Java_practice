3c8e53a6aa0367abd5f565cdebbba7d37a6af02c
Akanksha <akankshazc23@icloud.com>
2025-10-23 22:08:52 +0530

    Add Animal class to define basic animal behaviors including eating, roaming, and making noise

---
+++ hf_7/Animal.java
@@ -0,0 +1,15 @@
+package hf_7;
+
+public class Animal {
+    public void eat() {
+
+    }
+
+    public void roam() {
+
+    }
+
+    public void makeNoise() {
+        System.out.println("It hurts!!!");
+    }
+}
